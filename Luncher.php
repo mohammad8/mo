771463e6f677e35718b6a2a04199106ab508c936
mbmahdi <mbmahdi850@gmail.com>
2016-07-26 15:53:46 +0430

    Update Luncher.php

--- Luncher.php
+++ Luncher.php
@@ -1,5 +1,5 @@
 <?php
-define('BOT_TOKEN', '241266171:AAFUCTUAkWu6V-Kw5MCVhDtHniHfB3i5Et');
+define('BOT_TOKEN', '241266171:AAFUCTUAkWu6V-Kw5MCVhDtHniHfB3i5Etl');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
